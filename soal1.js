f9ec65f122dd3de2210668175a444ad14d3c2d25
pingkanpermata <yuko.IT.programming@gmail.com>
2023-03-21 16:58:15 +0700

    update

---
+++ soal1.js
@@ -0,0 +1,4 @@
+//var
+/* cakupan atau scope dari keyword var adalah global. maksudnya global disini adalah semua variabel
+yang dideklarasikan dengan var yang berada diluar function dapat diakses keseluruhan kode. keyword var yang
+nerada didalam suatu function maka hanya bisa diakses oleh function tersebut.*/
